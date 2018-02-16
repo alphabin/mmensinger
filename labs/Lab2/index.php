b75f2f2b4d924ce43ea109f0462fe9d5e9f7c3de
Matthew Mensinger <mmensinger@csumb.edu>
2018-02-16 01:21:04 +0000

    New commit

--- labs/Lab2/index.php
+++ labs/Lab2/index.php
@@ -30,4 +30,13 @@
 
 
     </body>
+    <br> <br> <br>
+    <footer>
+        CST 336 Internet Programming. 2018&copy; Mensinger <br />
+            <strong>Disclaimer:</strong> The information in this webpage is fictitous. <br />
+            It is used for academic purposes only.
+             <figure id="csumb">
+                <img src="img/csumb_logo.jpg" alt="California State University Monterey Bay logo" />
+            </figure>
+    </footer>
 </html>
