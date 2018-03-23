88594f0e24a06a589a6211ea8d588490b50d6b04
Matthew Mensinger <mmensinger@csumb.edu>
2018-03-23 01:35:11 +0000

    Lab5

--- labs/Lab6/index.php
+++ labs/Lab6/index.php
@@ -24,46 +24,71 @@
         
     }
     
-    function displaySearchResults() {
+    function displaySearchResults(){
         global $conn;
         
-        if(isset($_GET['searchForm'])) { //checks whether user has submitted the form
+        if (isset($_GET['searchForm'])) { //checks whether user has submitted the form
             
-            echo "<h3>Products Found: </h3>";
+            echo "<h3>Products Found: </h3>"; 
             
-            //The following sql works but it DOES NOT prevent SQL injection
-            //$sql = "SELECT * FROM `om_product` WHERE 1 AND productName LIKE '%".$_GET['product']."%'";
+            //following sql works but it DOES NOT prevent SQL Injection
+            //$sql = "SELECT * FROM om_product WHERE 1
+            //       AND productName LIKE '%".$_GET['product']."%'";
             
-            //Query below prevents SQL injection
-            $sql = "SELECT * FROM `om_product` WHERE 1";
+            //Query below prevents SQL Injection
             
             $namedParameters = array();
             
-            if(!empty($_GET['product'])) {
-                $sql .= "AND productName Like :productName";
-                $namedParameters[":productName"] = "%" . $_GET['product'];
-            }
+            $sql = "SELECT * FROM om_product WHERE 1";
             
-            if(!empty($_GET['category'])) {
-                $sql .= "AND catId Like :categoryId";
-                $namedParameters[":categoryId"] =  $_GET['product'];
+            if (!empty($_GET['product'])) { //checks whether user has typed something in the "Product" text box
+                 $sql .=  " AND productName LIKE :productName";
+                 $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
             }
+                  
+                  
+             if (!empty($_GET['category'])) { //checks whether user has typed something in the "Product" text box
+                 $sql .=  " AND catId = :categoryId";
+                 $namedParameters[":categoryId"] =  $_GET['category'];
+             }    
             
+             if (!empty($_GET['priceFrom'])) { //checks whether user has typed something in the "Product" text box
+                 $sql .=  " AND price >= :priceFrom";
+                 $namedParameters[":priceFrom"] =  $_GET['priceFrom'];
+             }
+             
+            if (!empty($_GET['priceTo'])) { //checks whether user has typed something in the "Product" text box
+                 $sql .=  " AND price <= :priceTo";
+                 $namedParameters[":priceTo"] =  $_GET['priceTo'];
+             }
+            
+             if (isset($_GET['orderBy'])) {
+                 
+                 if ($_GET['orderBy'] == "price") {
+                     
+                     $sql .= " ORDER BY price";
+                     
+                 } else {
+                     
+                      $sql .= " ORDER BY productName";
+                 }
+                 
+                 
+             }
             //echo $sql; //for debugging purposes
             
-            $namedParameters = array();
-            $namedParameters[":productName"] = "%" . $_GET['product'] . "%";
+             $stmt = $conn->prepare($sql);
+             $stmt->execute($namedParameters);
+             $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+            foreach ($records as $record) {
             
-            $stmt = $conn->prepare($sql);
-            $stmt->execute($namedParameters);
-            $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                 echo "<a href=\"purchaseHistory.php?productId=".$record["productId"]. "\"> History </a>";
+                 echo  $record["productName"] . " " . $record["productDescription"] . " $" . $record["price"] . "<br /><br />";
             
-            foreach($records as $record) {
-                
-                echo $record["productName"] . " " . $record["productDescription"] . "<br />";
-                
             }
         }
+        
     }
 
     
